7f41fff61bc7236f2df07aea456526634c24458d
Airstryk-1 <cj2027@yahoo.com>
2019-07-08 12:16:32 -0700

    minor changes

--- gaming-analytics-client/src/FilterBar/FilterBar.js
+++ gaming-analytics-client/src/FilterBar/FilterBar.js
@@ -1,19 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
 
-export default function FilterBar() {
-  return (
-    <div className='filters'>
-      <form className='filters-controls'>
-        <label htmlFor='print-type-filter'>Date:</label>
-        <select name='print-type-filter' id='print-type-filter'>
-          <option value='all'>All</option>
-          <option value='month'>Last 30 Days</option>
-          <option value='three months'>Last 90 Days</option>
-          <option value='six months'>Last 180 Days</option>
-          <option value='year'>Last Year</option>
-        </select>
-      </form>
-    </div>
-  )
-}
+// export default function FilterBar() {
+//   return (
+//     <div className='filters'>
+//       <form className='filters-controls'>
+//         <label htmlFor='print-type-filter'>Date:</label>
+//         <select name='print-type-filter' id='print-type-filter'>
+//           <option value='all'>All</option>
+//           <option value='month'>Last 30 Days</option>
+//           <option value='three months'>Last 90 Days</option>
+//           <option value='six months'>Last 180 Days</option>
+//           <option value='year'>Last Year</option>
+//         </select>
+//       </form>
+//     </div>
+//   )
+// }
